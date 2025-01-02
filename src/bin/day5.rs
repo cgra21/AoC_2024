ea17d650d0319d556f6fd211fc4245de40d74f0c
cgra21 <cjgranger2001@gmail.com>
2025-01-01 22:09:18 -0500

    Day 5 part 1

---
+++ src/bin/day5.rs
@@ -0,0 +1,81 @@
+use std::{collections::HashMap, env};
+
+use advent_of_code::utils;
+
+fn parse_input(input: &str) -> Vec<&str>{
+
+    let parts: Vec<&str> = input.split("\n\n").collect();
+
+    assert!(parts.len() == 2, "Input is not properly formatted for splitting.");
+
+    parts
+}
+
+fn generate_hashmap(rules: &str) -> HashMap<String, Vec<String>> {
+
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+
+    for rule in rules.lines() {
+
+        let (key, value) = rule.split_once('|').expect("Invalid rule");
+
+        map.entry(key.to_string())
+            .and_modify(|vec| vec.push(value.to_string()))
+            .or_insert_with(|| vec![value.to_string()]);
+    }
+
+    map
+}
+
+fn find_correct_updates(updates: &str, mapping: &HashMap<String, Vec<String>>) -> usize {
+
+    let updates = updates.lines();
+
+    let mut total = 0;
+
+    for update in updates {
+        let mut seen_pages: Vec<&str> = Vec::new();
+        let pages = update.split(',');
+
+        let mut valid = true;
+
+        for page in pages {
+            if let Some(after_pages) = mapping.get(page) {
+                if after_pages.iter().any(|p| seen_pages.contains(&p.as_str())) {
+                    valid = false;
+                    break;
+                } 
+            }
+            seen_pages.push(page);
+        }
+
+        if valid {
+            let middle_index = seen_pages.len() / 2;
+            let middle_page = seen_pages[middle_index];
+
+            total += middle_page.parse::<usize>().expect("Invalid page number");
+        }
+        
+
+    }
+
+    total
+}
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    let input = utils::read_input(file_path);
+
+    let parts = parse_input(&input);
+
+    let mapping = generate_hashmap(parts[0]);
+
+    let total = find_correct_updates(parts[1], &mapping);
+
+    println!("{total:?}");
+
+}
